de71a44813e10952be61ea4a2b1b57e26b2966e9
Federico Stra <stra.federico@gmail.com>
2023-04-27 10:40:30 +0200

    Add strict variants of `compMap` and `compMap'`

--- src/Composer.hs
+++ src/Composer.hs
@@ -17,7 +17,7 @@
 -- In addition there are 'compRev' and 'compRev'' which compose in reverse order.
 --
 -- This module also defines two functions, 'compMap' and 'compMap_', which build a composition
--- of functions parameterized by a list.
+-- of functions parameterized by a list, along with their strict counterparts 'compMap'' and 'compMap_''.
 module Composer
   ( -- * Simple composition
 
@@ -58,16 +58,19 @@ module Composer
     --
     -- respectively.
     --
-    -- As the name suggests, 'compMap' can also be defined in terms of 'comp' and 'map':
+    -- As the name suggests, 'compMap' can also be defined in terms of 'comp' and 'Prelude.map':
     --
-    -- > compMap f = comp . map f
+    -- @'compMap' f = 'comp' . 'Prelude.map' f@
     --
     -- On the other hand, 'compMap_' is simply obtained by pre-composing with a given function,
-    -- hence it can be defined as
+    -- hence it could be defined as
     --
-    -- > compMap_ f f0 = (. f0) . compMap f
+    -- @'compMap_' f f0 = (. f0) . 'compMap' f@
     compMap,
+    compMap',
     compMap_,
+    compMap_',
+    compMap'_,
   )
 where
 
@@ -92,15 +95,35 @@ compRev' = foldl' (flip (.)) id
 
 -- | Maps a list to a sequence of functions and composes them.
 --
+-- Implemented with 'foldr'.
 -- @'compMap' f@ is equivalent to @'compMap_' f id@.
 compMap :: (b -> (a -> a)) -> [b] -> (a -> a)
 compMap = flip . foldr
 
+-- | Maps a list to a sequence of functions and composes them.
+--
+-- Equivalent to 'compMap', but implemented with 'foldl''.
+compMap' :: (b -> (a -> a)) -> [b] -> (a -> a)
+compMap' = flip . foldl' . flip
+
 -- | Maps a list to a sequence of functions and composes them,
 -- pre-composing everything with an extra function.
+--
+-- Implemented with 'foldr'.
+-- @'compMap_' f f0@ is equivalent to @(. f0) . 'compMap' f@.
 compMap_ :: (b -> (a -> a)) -> (a -> a) -> [b] -> (a -> a)
 compMap_ f f0 = flip (foldr f . f0)
 
-{- Alternative implementations
-compMap_ f f0 = (. f0) . compMap f
--}
+-- | Maps a list to a sequence of functions and composes them,
+-- pre-composing everything with an extra function.
+--
+-- Equivalent to 'compMap_', but implemented with 'foldl''.
+compMap_' :: (b -> (a -> a)) -> (a -> a) -> [b] -> (a -> a)
+compMap_' f f0 = flip (foldl' (flip f) . f0)
+
+-- | Maps a list to a sequence of functions and composes them,
+-- pre-composing everything with an extra function.
+--
+-- This is just an alias of 'compMap_''.
+compMap'_ :: (b -> (a -> a)) -> (a -> a) -> [b] -> (a -> a)
+compMap'_ = compMap_'
